14986be46adc9ee26b29576830bf92485cee3d92
varadisthedev <varadisthedev@gmail.com>
2025-12-07 20:03:43 +0530

    updated the UI from the col style to a row, with glassmorphism

--- client/src/components/AddExpenses.jsx
+++ client/src/components/AddExpenses.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 
-
 export const AddExpenses = () => {
-    const [amount, setAmount] = useState("");
+  const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
   const [date, setDate] = useState("");
   const [note, setNote] = useState("");
@@ -13,54 +12,65 @@ export const AddExpenses = () => {
   };
   return (
     <>
-      <form onSubmit={handleSubmit}
-      className="flex flex-col gap-4 p-6 bg-white rounded-lg shadow-md max-w-md mx-auto mt-10"
-    >
-        <label className="text-gray-700 font-medium">Enter Amount :</label>
-        <input type="text" placeholder="enter amount" value={amount} onChange={(e) => setAmount(e.target.value)}
-        className="border rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-green-400"
-/>
-        <label className="text-gray-700 font-medium">Category:</label>
-      <select
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-        className="border rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-green-400"
-      
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-row flex-wrap gap-8 p-10 backdrop-blur-xl bg-white/20 border border-white/30 rounded-2xl shadow-xl max-w-6xl mx-auto mt-10 items-end
+        bg-linear-to-br before:from-white/20 before:to-transparent"
       >
-        <option value="">Select</option>
-        <option value="Food">Food</option>
-        <option value="Travel">Travel</option>
-        <option value="Shopping">Shopping</option>
-        <option value="Entertainment">Entertainment</option>
-        <option value="Others">Others</option>
-      </select>
-      <label className="text-gray-700 font-medium">Date:</label>
-      <input
-        type="date"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-         className="border rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-green-400"
-      />
-      <label className="text-gray-700 font-medium">Description:</label>
-      <input
-        type="text"
-        placeholder="Optional"
-        value={note}
-        onChange={(e) => setNote(e.target.value)}
-         className="border rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-green-400"
-      />
-       <button
+        <div className="w-36">
+          <label className="text-white/95 font-medium">Enter Amount :</label>
+          <input
+            type="text"
+            placeholder="amount in INR"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            className="border rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-green-400"
+          />
+        </div>
+
+        <div className="w-36">
+          {" "}
+          <label className="text-white/95 font-medium">Category:</label>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="border rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-green-400"
+          >
+            <option value="">Select</option>
+            <option value="Food">Food</option>
+            <option value="Travel">Travel</option>
+            <option value="Shopping">Shopping</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Others">Others</option>
+          </select>
+        </div>
+
+        <div className="w-56">
+          <label className="text-white/95 font-medium">Date:</label>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            className="border rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-green-400"
+          />
+        </div>
+
+        <div className="w-56">
+          <label className="text-white/95 font-medium">Description:</label>
+          <input
+            type="text"
+            placeholder="Optional"
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+            className="border rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-green-400"
+          />
+        </div>
+        <button
           type="submit"
-          style={{
-            padding: "8px 16px",
-            backgroundColor: "#4CAF50",
-            color: "white",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-          }}
+          className="px-6 py-3 bg-emerald-400/80 text-white font-semibold rounded-xl shadow-md shadow-emerald-500/40 
+          hover:bg-emerald-400 transition-all duration-200"
         >
-          Add Expense
+          Add
         </button>
       </form>
     </>
