a96fdffb4cfd8d59a83b5f99f833cca4e53be675
varadisthedev <varadisthedev@gmail.com>
2025-12-07 20:05:21 +0530

    chore: imported components and added a bg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,9 @@ import { RecentExpenses } from "./components/RecentExpenses";
 
 function App() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-linear-to-br from-slate-800 via-sky-900 to-indigo-900">
+      {/* bg-gray-50 */}
+      <div className="flex flex-col items-center justify-center min-h-screen ">
         <h1 className="text-3xl font-bold mb-2">1Pocket</h1>
         <p className="text-gray-600 mb-4">All your expenses, one place.</p>
         <p className="bg-orange-500 text-white px-4 py-1 rounded">
@@ -16,8 +17,12 @@ function App() {
         </p>
 
         <AddExpenses />
+
+        <div className="flex flex-row items-center justify-center gap-44">
+          <RecentExpenses />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
